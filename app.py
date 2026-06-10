dbdfe6c5fe25f7bcc1853311cee69f5d65305899
Callen :D <callen.lin1@education.nsw.gov.au>
2026-06-10 21:33:30 +0000

    setup: intial flask project and fix devcontainer

--- app.py
+++ app.py
@@ -1,11 +1,14 @@
-from flask import Flask, render_template 
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 # Homepage for flask
+
+
 @app.route("/")
 def homepage():
-  return render_template('index.html')
+    return render_template('index.html')
+
 
 # Allows directly running of flask program with python3 app.py
 if __name__ == '__main__':
